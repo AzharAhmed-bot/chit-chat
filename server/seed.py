3d8f7b69124e3734735d09455c60c7c3c76d4d5d
noorzakiya <zakiyanoorsalan@gmail.com>
2025-04-06 19:30:26 +0300

    🚧seeding data for messages

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,7 @@
 from app import db,app
 from model import User, Chat, Message, MessageStatus, ChatMembers
 import uuid
-
+from datetime import datetime
 
 #SESSION
 
@@ -9,9 +9,14 @@ def seed():
     user_1=User(id=str(uuid.uuid4()), name="Alice", phone_number="+254773470576")
     user_2=User(id=str(uuid.uuid4()), name="Bob", phone_number="+254773470576")
     user_3=User(id=str(uuid.uuid4()), name="Charlie", phone_number="+254773470576")
+    message_1=Message(id=str(uuid.uuid4()),user_id=user_1.id,chat_id=Chat.id,type="text",content='Hey girl',sent_at=datetime.utcnow())
+    message_2=Message(id=str(uuid.uuid4()),user_id=user_2.id,chat_id=Chat.id,type="text",content='girllly',sent_at=datetime.utcnow())
+     
+    message_status1=MessageStatus(user_id=User.id,message_id=Message.id,delivered_at=datetime.utcnow(),seen_at=datetime.utcnow() )
 
-    db.session.add_all([user_1, user_2, user_3])
+    db.session.add_all([user_1, user_2, user_3,message_1,message_2])
     db.session.commit()
+   
 
 if __name__=="__main__":
     with app.app_context():
