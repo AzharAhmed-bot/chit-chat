5216532a516064991ac07fbe408e7446c52d3dd0
azhar <azharahmedtakoy4444@gmail.com>
2025-04-16 21:55:04 +0300

    Experimenting on live communication

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState,useEffect } from 'react'
+import { io } from 'socket.io-client'
 
+const socket=io('http://localhost:5000')
 function App() {
-  const [count, setCount] = useState(0)
+  const getOtpStatus=()=>{
+    const otpStatus=localStorage.getItem('otp_requested')
+    if(otpStatus){
+      return true
+    }
+    return false
+  }
+  const [requestOtp, setRequestOtp] = useState(getOtpStatus())
+  const [phoneNumber,setPhoneNumber]=useState(null)
+  const [otp,setOtp]=useState(null)
 
+  
+  const handleSendOTP=async()=>{
+    const response=await fetch('http://localhost:5000/login',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        mode:"request",
+        phone_number:phoneNumber
+      })
+
+    })
+    if(response.status===200){
+      const data=await response.json()
+      localStorage.setItem('otp_requested',true)
+      setRequestOtp(localStorage.getItem('otp_requested'))
+      console.log(data)
+    }else{
+      const data=await response.json()
+      console.log(data)
+    }
+    
+  }
+  const handleVerifyOTP=async()=>{
+    const response=await fetch('http://localhost:5000/login',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        mode:"verify",
+        phone_number:phoneNumber,
+        otp:otp
+      })
+  })
+  const data=await response.json()
+  console.log(data)
+  localStorage.removeItem('otp_requested')
+  }
   return (
-    <>
+   <div>
+    <label htmlFor="phone_number">Enter phone Number</label>
+    <input type="text" id="phone_number" name="phone_number" onChange={(e)=>setPhoneNumber(e.target.value)} />
+    {requestOtp ?( 
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input type="text" id="otp" name="otp" onChange={(e)=>setOtp(e.target.value)} placeholder="Enter OTP" />
+      <button onClick={handleVerifyOTP}>Verify</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      ):
+      <button onClick={handleSendOTP}>Request OTP</button> 
+      } 
+     
+   </div> 
   )
 }
 
